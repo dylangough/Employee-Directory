7e8983c80ba537696c0c950db11a5a55e2f8d5a8
Dylan Gough <dylangough35@gmail.com>
2020-09-23 00:35:48 -0400

    MAJOR CHANGING

--- employee-directory/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Header = () => {
-    return (
-        <header className="">
-            <h3>Employees:</h3>
-        </header>
-    )
-}
-export default Header;
--- employee-directory/src/pages/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Table from "../components/Table";
-export default function Search() {
-    return (
-        <main>
-            <Table />
-        </main>
-    )
-}
